0dc5ebd8c9f511fad100d0c77a2fe4a1d0997ca7
Janis <janis.paberzs18@gmail.com>
2023-10-20 00:21:59 +0300

    edit 06 page

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 # 0049Pera
  
+1. <https://megafronteam.github.io/0049Pera/public>
+2. <https://megafronteam.github.io/0049Pera/public/06-contact.html>
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -30,15 +30,57 @@ function eventHandler() {
 	});
 
 
+	gsap.registerPlugin(ScrollTrigger);
 	let scroller = document.querySelector(".scroller");
-
+	let bodyScrollBar;
 	if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-		Scrollbar.init(scroller, {
+		bodyScrollBar = Scrollbar.init(scroller, {
 			damping: .1,
 			thumbMinSize: 20,
 			alwaysShowTracks: true,
 		});
 	};
+
+	if (bodyScrollBar) {
+		ScrollTrigger.scrollerProxy(scroller, {
+			scrollTop(value) {
+				if (arguments.length) {
+					bodyScrollBar.scrollTop = value;
+				}
+				return bodyScrollBar.scrollTop;
+			},
+		});
+
+		bodyScrollBar.addListener(ScrollTrigger.update);
+	}
+
+
+	gsap.utils.toArray("[data-aos]").forEach(aos => {
+
+
+		const animate = aos.dataset.animate;
+		function myfunction() {
+			aos.classList.add(`aos-animate`);
+			if (animate) {
+				aos.classList.toggle(animate);
+			}
+		};
+		const rect = aos.getBoundingClientRect();
+		ScrollTrigger.create({
+			scroller: scroller,
+			trigger: aos,
+			start: 'top bottom',
+			end: 'bottom +100 top',
+
+			// markers: true,
+			toggleActions: "play none none none",
+			onEnter: () => myfunction(),
+			// onLeave: () => myfunction(),
+			// onLeaveBack: () => myfunction(),
+			// onEnterBack: () => myfunction(),
+			invalidateOnRefresh: true,
+		});
+	})
 
 	new Swiper('.sContact__swiper--js .swiper', {
 		// slidesPerView: 'auto',
@@ -54,6 +96,48 @@ function eventHandler() {
 			enabled: true,
 		},
 	});
+
+	AOS.init();	
+
+	document.addEventListener('aos:in', ({ detail }) => {
+		console.log('animated in', detail);
+	});
+
+	function inputFile(){
+		if (document.querySelector('.upload-field')){
+			let uploadField = document.querySelectorAll('.upload-field');
+			for (let i=0;i<uploadField.length;i++){
+				let inputFile = uploadField[i].querySelector('.input-upload');
+				inputFile.addEventListener('change',() => uploadField[i].querySelector('.upload-field__file-name').innerHTML = inputFile.files[0].name);
+			}
+		}
+	}
+	inputFile();
+
+	let stickyElem = document.querySelector('.sticky-content');
+	if (stickyElem) {
+		let stickyNav = new hcSticky(stickyElem, {
+			stickTo: $('.sticky-wrap'),
+			top: 80,
+			mobileFirst: true,
+			disable: true,
+			responsive: {
+				992: {
+					disable: false,
+				}
+			}
+		});
+	
+		ScrollTrigger.create({
+			scroller: scroller,
+			// trigger: stickyNav,
+			onUpdate: (self) => {
+				// console.log(self);
+				// stickyNav.attach();
+				stickyNav.refresh();
+			},
+		})
+	}
 
 };
 if (document.readyState !== 'loading') {
